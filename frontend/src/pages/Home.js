756c88e79bbeff634a5e7070b74ce07fc5d2a8da
Sanjay-S31 <sanjaydonn007@gmail.com>
2024-04-24 00:58:01 +0530

    carousel added

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -9,32 +9,33 @@ import carousel_img4 from '../assets/carousel_img4.jpeg'
 
 export default function Home(){
 
-    const [currentSlide, setCurrentSlide] = useState(0);
+    const [slide, setSlide] = useState(0)
 
     const nextSlide = () => {
-        setCurrentSlide((prevSlide) => (prevSlide === 3 ? 0 : prevSlide + 1));
-    };
+        setSlide((slide + 1) % 4)
+    }
 
     const prevSlide = () => {
-        setCurrentSlide((prevSlide) => (prevSlide === 0 ? 3 : prevSlide - 1));
-    };
+        setSlide((slide - 1 + 4) % 4)
+    }
+
 
     return (
         <div className="homepage">
             <h1>Welcome to CraftsConnect</h1>
-            <BsArrowLeftCircleFill className='arrow arrow-left' onClick={prevSlide}/>
+            <BsArrowLeftCircleFill className='arrow arrow-left' onClick={prevSlide} />
             <div className='carousel'>
-                <img className="slides" src={carousel_img1} alt="img1" style={{ transform: `translateX(-${currentSlide * 100}%)` }} />
-                <img className="slides" src={carousel_img2} alt="img2" style={{ transform: `translateX(-${currentSlide * 100}%)` }} />
-                <img className="slides" src={carousel_img3} alt="img3" style={{ transform: `translateX(-${currentSlide * 100}%)` }} />
-                <img className="slides" src={carousel_img4} alt="img4" style={{ transform: `translateX(-${currentSlide * 100}%)` }} />
+                <img className="slides" src={carousel_img1} width={600} height={400} alt="img1" style={{ transform: `translateX(-${slide * 100}%)` }} />
+                <img className="slides" src={carousel_img2} width={600} height={400} alt="img2" style={{ transform: `translateX(-${slide * 100}%)` }} />
+                <img className="slides" src={carousel_img3} width={600} height={400} alt="img3" style={{ transform: `translateX(-${slide * 100}%)` }} />
+                <img className="slides" src={carousel_img4} width={600} height={400} alt="img4" style={{ transform: `translateX(-${slide * 100}%)` }} />
             </div>
-            <BsArrowRightCircleFill className='arrow arrow-right' onClick={nextSlide}/>
+            <BsArrowRightCircleFill className='arrow arrow-right' onClick={nextSlide} />
             <span className='indicators'>
-                <button className={`indicator ${currentSlide === 0 ? 'active' : ''}`} onClick={() => setCurrentSlide(0)} ></button>
-                <button className={`indicator ${currentSlide === 1 ? 'active' : ''}`} onClick={() => setCurrentSlide(1)} ></button>
-                <button className={`indicator ${currentSlide === 2 ? 'active' : ''}`} onClick={() => setCurrentSlide(2)} ></button>
-                <button className={`indicator ${currentSlide === 3 ? 'active' : ''}`} onClick={() => setCurrentSlide(3)} ></button>
+                <button className={`indicator ${slide === 0 ? 'active' : ''}`} onClick={() => setSlide(0)} ></button>
+                <button className={`indicator ${slide === 1 ? 'active' : ''}`} onClick={() => setSlide(1)} ></button>
+                <button className={`indicator ${slide === 2 ? 'active' : ''}`} onClick={() => setSlide(2)} ></button>
+                <button className={`indicator ${slide === 3 ? 'active' : ''}`} onClick={() => setSlide(3)} ></button>
             </span>
         </div>
     )
