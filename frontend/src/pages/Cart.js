d6d7009ed48ecf47d0547746d3f6c8e33827d0a7
Sanjay-S31 <sanjaydonn007@gmail.com>
2024-05-19 18:02:11 +0530

    Sweet alert added

--- frontend/src/pages/Cart.js
+++ frontend/src/pages/Cart.js
@@ -1,12 +1,16 @@
 import { useState, useEffect } from "react";
 import { useAuthContext } from "../hooks/useAuthContext";
 import { FaRupeeSign } from 'react-icons/fa'
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
+import { useNavigate } from 'react-router-dom'
 import './style_pages/cart.css'
 
 export default function Cart() {
     const { user } = useAuthContext()
     const [cartProduct, setCartProduct] = useState([])
     const [total,setTotal] = useState(0)
+    const navigate = useNavigate()
 
     useEffect(() => {
         async function displayAll(){
@@ -52,7 +56,7 @@ export default function Cart() {
                     'Content-Type': 'application/json',
                     'Authorization': `Bearer ${user.token}`
                 }
-            });
+            })
 
             if (response.ok) {
                 const updatedCart = cartProduct.filter(item => item._id !== id);
@@ -63,6 +67,37 @@ export default function Cart() {
             }
         } catch (error) {
             console.error("Purchase error:", error);
+        }
+    }
+
+    const showSwal = () => {
+        withReactContent(Swal).fire({
+            icon: 'success',
+            title: 'Thank You!',
+            text: 'Your order has been placed successfully. We appreciate your purchase!',
+            confirmButtonText: 'Continue Shopping'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                removeCart()
+                navigate('/')
+            }
+        })
+    }
+
+    const removeCart = async () => {
+        const response = await fetch('/api/cart/remove' , {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${user.token}`
+            }
+        })
+
+        if(response.ok){
+            console.log("Removed successfully")
+        }
+        else{
+            console.log("Error occured")
         }
     }
 
@@ -79,7 +114,7 @@ export default function Cart() {
             ))}
             <div className="checkout-container">
                 <h3 className="total-amount">Total Amount : <FaRupeeSign/>{total}</h3>
-                <button className="checkout-button">Checkout</button>
+                <button className="checkout-button" onClick={showSwal}>Checkout</button>
             </div>
         </div>
     );
